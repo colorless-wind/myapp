fae223b6436b329cec7e0d1fbcde7b436d87b799
blue-eyes <e3156bios@163.com>
2018-07-12 17:32:23 +0800

    add PM2

--- public/js/MessageShowByPlane.js
+++ public/js/MessageShowByPlane.js
@@ -9,26 +9,26 @@ var succCallBack = function (res) {
 
         for (var i in pbdArr) {
             if (i < 3) {
-                $('#pbdList').find('ul').append('<li class="origin">' + (i-(-1)) + '、 ' + pbdArr[i].value + '</li>');
+                $('#pbdList').find('ul').append('<li class="origin">' + (i - (-1)) + '、 ' + pbdArr[i].value + '</li>');
             } else {
                 $('#pbdList').parent().find('.more img').show();
-                $('#pbdList').find('ul').append('<li class="rest" style="display:none;">' + (i-(-1)) + '、 ' + pbdArr[i].value + '</li>');
+                $('#pbdList').find('ul').append('<li class="rest" style="display:none;">' + (i - (-1)) + '、 ' + pbdArr[i].value + '</li>');
             }
         }
         for (var i in wlArr) {
             if (i < 3) {
-                $('#wlList').find('ul').append('<li class="origin">' + (i-(-1)) + '、 ' + wlArr[i].value + '</li>');
+                $('#wlList').find('ul').append('<li class="origin">' + (i - (-1)) + '、 ' + wlArr[i].value + '</li>');
             } else {
                 $('#wlList').parent().find('.more img').show();
-                $('#wlList').find('ul').append('<li class="rest" style="display:none;">' + (i-(-1)) + '、 ' + wlArr[i].value + '</li>');
+                $('#wlList').find('ul').append('<li class="rest" style="display:none;">' + (i - (-1)) + '、 ' + wlArr[i].value + '</li>');
             }
         }
         for (var i in otherArr) {
             if (i < 3) {
-                $('#otherList').find('ul').append('<li class="origin">' + (i-(-1)) + '、 ' + otherArr[i].value + '</li>');
+                $('#otherList').find('ul').append('<li class="origin">' + (i - (-1)) + '、 ' + otherArr[i].value + '</li>');
             } else {
                 $('#otherList').parent().find('.more img').show();
-                $('#otherList').find('ul').append('<li class="rest" style="display:none;">' + (i-(-1)) + '、 ' + otherArr[i].value + '</li>');
+                $('#otherList').find('ul').append('<li class="rest" style="display:none;">' + (i - (-1)) + '、 ' + otherArr[i].value + '</li>');
             }
         }
         if (pbdArr.length == 0) {
@@ -55,22 +55,10 @@ $.ajax(url, {
     //            beforeSend: beforeSend,
     //            complete: doComplete,
     //            error: doErrCallBack,
-    success: succCallBack
-    /*function (res) {
-            data = res.resultojbect;
-            //是否显示输出按钮
-            if (data.pbd.length < 2) {
-                $('#delPBD').hide();
-            }
-            if (data.wl.length < 2) {
-                $('#delWL').hide();
-            }
-            if (data.others.length < 2) {
-                $('#delOthers').hide();
-            }
-
-
-        }*/
+    success: succCallBack,
+    error: function (err) {
+        console.warn(err);
+    }
 })
 
 $('.more').on('tap', function () {
